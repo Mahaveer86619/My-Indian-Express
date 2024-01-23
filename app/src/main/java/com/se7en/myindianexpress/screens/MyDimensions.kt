73011fa93a958710239c57fdc5dc61a96cf20a08
Mahaveer Prasad Panigrahi <114766319+Mahaveer86619@users.noreply.github.com>
2024-01-23 23:26:04 +0530

    chore: Added a file to store commonly used values

---
+++ app/src/main/java/com/se7en/myindianexpress/screens/MyDimensions.kt
@@ -0,0 +1,13 @@
+package com.se7en.myindianexpress.screens
+
+import androidx.compose.ui.unit.dp
+
+object MyDimensions {
+
+    val MediumPadding1 = 24.dp
+    val MediumPadding2 = 30.dp
+    val IndicatorSize = 14.dp
+
+    val PageIndicatorWidth = 52.dp
+
+}
